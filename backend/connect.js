8b83b15775684b790ad1323ab5209d726ce02c95
figopratama <socialmedia.figo@gmail.com>
2026-06-12 13:00:59 +0700

    fix: cors allow localhost:5173, use .env path

--- backend/connect.js
+++ backend/connect.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require("dotenv").config({path: "./config.env"})
+require("dotenv").config({path: "./.env"})
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const upload = multer()
 const app = express()
 const PORT = 3000
 
-app.use(cors())
+app.use(cors({origin: "http://localhost:5173", credentials: true}))
 app.use(express.json())
 app.use(upload.any())
 app.use(posts)
